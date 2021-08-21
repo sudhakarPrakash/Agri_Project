79b945355b763a04a0b33e6b2ac4683520fc2054
sudhakarPrakash <54389929+sudhakarPrakash@users.noreply.github.com>
2021-08-21 20:03:40 +0530

    Update predict_yield.py

--- predict_yield.py
+++ predict_yield.py
@@ -1,8 +1,9 @@
 import sys
 import json
 # import pandas as pd
-import numpy as np
 import joblib
+import numpy as np
+# import joblib
 # import sklearn
 # import matplotlib.pyplot as plt
 # import seaborn as sns
